9536e8002d6e29f3f074b8028689cdcf4516500f
Christopher Tran <tran.christopher09@gmail.com>
2023-10-07 19:30:48 +0000

    Completed prep-javascript-conditionals exercise.

---
+++ prep-javascript-conditionals/main.js
@@ -0,0 +1,64 @@
+/* Your JS goes in this file */
+const age = 25;
+function isAdult(age) {
+  if (age < 18) {
+    return false;
+  } else {
+    return true;
+  }
+}
+console.log(isAdult(age));
+
+const student1Score = 20;
+function didStudentPass(score) {
+  if (score >= 70) {
+    return true;
+  } else {
+    return false;
+  }
+}
+console.log(didStudentPass(student1Score));
+
+const student2Score = 75;
+function gradeCalculator(score) {
+  if (score < 60) {
+    return 'F';
+  } else if (score < 70 && score > 60) {
+    return 'D';
+  } else if (score < 80 && score > 70) {
+    return 'C';
+  } else if (score < 90 && score > 80) {
+    return 'B';
+  } else if (score < 100 && score > 90) {
+    return 'A';
+  } else {
+    return 'A++';
+  }
+}
+console.log(gradeCalculator(student2Score));
+
+const season = 'summer';
+function seasonMessenger(season) {
+  if (season === 'summer') {
+    return "it's hot today";
+  } else if (season === 'spring') {
+    return 'the flowers are blooming';
+  } else if (season === 'autumn') {
+    return 'the leaves are changing colors';
+  } else if (season === 'winter') {
+    return "it's cold today";
+  } else {
+    return 'please enter a valid season';
+  }
+}
+console.log(seasonMessenger(season));
+
+const dayOfTheWeek = 'saturday';
+function dayDetector(dayOfTheWeek) {
+  if (dayOfTheWeek === 'saturday' || dayOfTheWeek === 'sunday') {
+    return 'have a good weekend!';
+  } else {
+    return "It's a weekday!";
+  }
+}
+console.log(dayDetector(dayOfTheWeek));
---
+++ prep-javascript-conditionals/prep-javascript-conditionals-quiz-notes.md
@@ -0,0 +1,45 @@
+# prep-javascript-conditionals-quiz-notes
+
+## Quiz Questions
+
+After completing this exercise, you should be able to discuss or answer the following questions:
+
+- What does `strictly equal` mean?
+
+-same value and data type
+
+- What is the logical **and** operator?
+
+-&&
+
+- Can you name some comparison operators?
+
+the less than <, greater than >, strictly equal to ===
+
+## Notes
+
+All student notes should be written here.
+
+How to write `Code Examples` in markdown
+
+for JS:
+
+```javascript
+const data = 'Howdy';
+```
+
+for HTML:
+
+```html
+<div>
+  <p>This is text content</p>
+</div>
+```
+
+for CSS:
+
+```css
+div {
+  width: 100%;
+}
+```
